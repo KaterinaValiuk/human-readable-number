3445be1427189823ad7deb9b4fa9949732d29214
KaterinaValiuk <e5054404@gmail.com>
2021-08-13 15:11:24 +0300

    docs: started to solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
+    const hundredsNum = {
+        1 : 'one hundred',
+        2 : 'two hundred',
+        3 : 'three hundred',
+        4 : 'four hundred',
+        5 : 'five hundred',
+        6 : 'six hundred',
+        7 : 'seven hundred',
+        8 : 'eight hundred',
+        8 : 'nine hundred',
+    };
   
 }
